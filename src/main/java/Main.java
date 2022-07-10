85b8f2cabc1bb364a3459193ddf19cbf8b0a8eee
Git-Yee <git-yee@ZipcodersMBP58.fios-router.home>
2022-07-10 11:44:29 -0400

    worked on lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pick a number");
+        int n = scanner.nextInt();
 
+        int temp = 0;
+        for (int i = 1; i <= n; i++) {
+            temp = temp + i;
+        }
+        System.out.println(temp);
     }
 }
